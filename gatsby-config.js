b37e946dd0c6f3045ae96f425a9af9d552fba2d7
Bradley Herman <bradley.t.herman@gmail.com>
2025-01-24 09:50:15 -0800

    Yanr

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,8 @@ module.exports = {
       },
     },
     `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -33,8 +35,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
