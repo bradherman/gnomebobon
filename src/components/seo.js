b1cd3b65bc23ef20c59f4dc73c0b6c2db452df43
Bradley Herman <bradley.t.herman@gmail.com>
2025-01-24 09:54:32 -0800

    Remove stuff

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,30 +1,8 @@
-/**
- * SEO component that queries for data with
- * Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
-
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 
-function Seo({ description, title, children }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+function Seo({ title, children }) {
+  const metaDescription = "Building cool shit"
+  const defaultTitle = "Gnome Bobon"
 
   return (
     <>
@@ -34,7 +12,7 @@ function Seo({ description, title, children }) {
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:creator" content={site.siteMetadata?.author || ``} />
+      <meta name="twitter:creator" content="Gnome Bobon" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={metaDescription} />
       {children}
